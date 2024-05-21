deeec085ad94d081a7e548c6908e6752aca1efb7
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-05-21 22:55:02 +0530

    setup environment variables & update gitignore

---
+++ env.js
@@ -0,0 +1,9 @@
+const env = {
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    apiEndpoint: import.meta.env.VITE_API_ENDPOINT,
+    dbId: import.meta.env.VITE_DB_ID,
+    collectionId: import.meta.env.VITE_BLOG_COLLECTION_ID,
+    bucketId: import.meta.env.VITE_BUCKET_ID
+}
+
+export default env
