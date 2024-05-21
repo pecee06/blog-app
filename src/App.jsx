3f6ae1ab1269f8ba2cd4a8fa082fe4be6472f991
pecee06 <priyanshu.choudhury2406@outlook.com>
2024-05-21 21:59:23 +0530

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { Outlet } from "react-router-dom"
+
 const App = () => {
   return (
-    <div>App</div>
+    <Outlet/>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+)
